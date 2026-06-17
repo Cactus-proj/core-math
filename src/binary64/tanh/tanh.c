5778ea05b8df0ff18e920f522c39a5139e1a0b80
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2026-06-17 07:46:44 +0200

    [tanh] added new worst cases

--- src/binary64/tanh/tanh.c
+++ src/binary64/tanh/tanh.c
@@ -345,7 +345,7 @@ double cr_tanh(double x){
     double p = dx*((ch[0] + dx*ch[1]) + dx2*(ch[2] + dx*ch[3]));
     double rh = th*sp.f;
     rh += (p + ((2*0x1.3p-55)*ax))*rh;
-    // fails with e = rh*0x1.f9p-50 and x=0x1.09c53f70fe242p+2 (rndz, with/without fma contraction)
+    // fails with e = rh*0x1.fap-50 and x=0x1.09cc2de69e78cp+2 (rndu, with/without fma contraction)
     double e = rh*0x1.1p-49;
     rh = (2*rh)/(1 + rh);
     double one = __builtin_copysign(1,x);
